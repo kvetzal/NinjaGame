f2287609935684425cbdc6c64a7352be4e8bfb68
corrigan-stephanie <corrigan_stephanie@hotmail.com>
2013-08-12 13:49:02 -0400

    Added code to prevent unity from trying to access the enemy after it has been destroyed

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -25,55 +25,61 @@ public class Enemy : MonoBehaviour {
 	
 	private void enemyConstructor() {
 		for (int i = 0; i < Enemies.Count; i++) {
-			Enemies[i] = (GameObject)Instantiate(enemyPrefab);
-			Enemies[i].rigidbody.isKinematic = true;
-			Enemies[i].transform.position = new Vector3(2f, 2f, 0);
+			if (Enemies[i] != null) {
+				//Enemies[i] = (GameObject)Instantiate(enemyPrefab);
+				Enemies[i].rigidbody.isKinematic = true;
+				Enemies[i].transform.position = new Vector3(2f, 2f, 0);
+			}
 		}
 	}
 	
 	private void enemyUpdate() {
 		foreach (GameObject enemy in Enemies) {
-			//enemy.transform.position = new Vector3(2f, 2f, 0);
-			//math example
-			//float num = Mathf.Sin(Mathf.PI * 90 / 180);
-			// returns 1
-			
-			for (int i = 0; i < lines.Count; i++) {
-				if (lines[i] != null) {
-					Destroy(lines[i].gameObject);
+			if (enemy != null) {
+				//enemy.transform.position = new Vector3(2f, 2f, 0);
+				//math example
+				//float num = Mathf.Sin(Mathf.PI * 90 / 180);
+				// returns 1
+				
+				for (int i = 0; i < lines.Count; i++) {
+					if (lines[i] != null) {
+						Destroy(lines[i].gameObject);
+					}
 				}
-			}
-			
-			const int ENDINGDEGREE = -45;
-			int degree = 45;
-			
-			do {
-				/*Debug.Log (rayVector);
-				Mathf works in radians
-				see figure 1*/
 				
-				double rayVectorX = Mathf.Cos(Mathf.PI * degree / 180);
-				double rayVectorY = Mathf.Sin(Mathf.PI * degree / 180);
-				Vector3 rayVector = new Vector3((float)rayVectorX, (float)rayVectorY, 0f);
-				Ray ray = new Ray(enemy.transform.position, rayVector);
+				const int ENDINGDEGREE = -45;
+				int degree = 45;
 				
-				LineRenderer line = (LineRenderer)Instantiate(linePrefab);
-				line.SetPosition(0, enemy.transform.position);
+				do {
+					/*Debug.Log (rayVector);
+					Mathf works in radians
+					see figure 1*/
+					
+					double rayVectorX = Mathf.Cos(Mathf.PI * degree / 180);
+					double rayVectorY = Mathf.Sin(Mathf.PI * degree / 180);
+					Vector3 rayVector = new Vector3((float)rayVectorX, (float)rayVectorY, 0f);
+					Debug.Log ("enemy exist: " + enemy);
+					Ray ray = new Ray(enemy.transform.position, rayVector);
+					
+					LineRenderer line = (LineRenderer)Instantiate(linePrefab);
+					line.SetPosition(0, enemy.transform.position);
+					
+					line.SetPosition(1, ray.GetPoint(7));
+					Debug.Log ("Ray.GetPoint(7).x : " + ray.GetPoint(7).x);
+					lines.Add(line);
+					
+					degree -= 5;
+					visionRays.Add(ray);
+				}while(degree > ENDINGDEGREE);
 				
-				line.SetPosition(1, ray.GetPoint(7));
-				lines.Add(line);
-				
-				degree -= 5;
-				visionRays.Add(ray);
-			}while(degree > ENDINGDEGREE);
-			
-			foreach (Ray ray in visionRays) {
-				RaycastHit hit;
-				if (Physics.Raycast(ray, out hit, ray.GetPoint(7).x)) {
-	
-					if (hit.collider.tag == "Ninja") {
-						count++;
-						Debug.Log("Ninja was seen " + count + " times");
+				foreach (Ray ray in visionRays) {
+					RaycastHit hit;
+					if (Physics.Raycast(ray, out hit, ray.GetPoint(7).x)) {
+		
+						if (hit.collider.tag == "Ninja") {
+							count++;
+							Debug.Log("Ninja was seen " + count + " times");
+						}
 					}
 				}
 			}
@@ -81,19 +87,25 @@ public class Enemy : MonoBehaviour {
 	}
 	
 	private void GameStart() {
-		for (int i  = 0; i < Enemies.Count; i++) {
-			Destroy(Enemies[i]);
+		for (int i = 0; i < Enemies.Count; i++) {
+			if (Enemies[i] != null) {
+				Destroy(Enemies[i].gameObject);
+			}
 		}
-		Enemies.Add(new GameObject("Enemy"));
+		Enemies.Add((GameObject)Instantiate(enemyPrefab));
 		enemyConstructor();
 		foreach (GameObject enemy in Enemies) {
-			enemy.rigidbody.isKinematic = false;
+			if (enemy != null) {
+				enemy.rigidbody.isKinematic = false;
+			}
 		}
 	}
 	
 	private void GameOver() {
 		foreach (GameObject enemy in Enemies) {
-			enemy.rigidbody.isKinematic = true;
+			if (enemy != null) {
+				enemy.rigidbody.isKinematic = true;
+			}
 		}
 	}
 }
